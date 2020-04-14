90b16c1edb4a67e2b4c3edc1e529e1f3e97c8134
Kyle Johnson <kylejohnson@Kyles-Air.fios-router.home>
2020-04-14 19:54:32 -0400

    commit number 1

---
+++ orbit-report/src/app/satellite.ts
@@ -0,0 +1,8 @@
+export class Satellite {
+name: string;
+orbitType: string;
+type: string;
+operational: boolean
+launchdate: string;
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {}
+};
