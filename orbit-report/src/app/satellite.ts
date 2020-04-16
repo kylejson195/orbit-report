54c63b2d163ffaf8f15c781516646811e9e033a4
Kyle Johnson <kylejohnson@Kyles-Air.fios-router.home>
2020-04-16 11:41:32 -0400

    Up to part 6

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -3,6 +3,15 @@ name: string;
 orbitType: string;
 type: string;
 operational: boolean
-launchdate: string;
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {}
+launchDate: string;
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    this.name = name,
+    this.type = type,
+    this.launchDate = launchDate,
+    this.orbitType = orbitType,
+    this.operational = operational
+}
+shouldShowWarning() {
+    return true
+}
 };
